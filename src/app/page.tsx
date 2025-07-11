00ecccf055b24500aa5da7e703bf2bc4302e00f8
zhouxiao <zhouxiao@npn.sg>
2025-07-11 15:35:43 +0800

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from '@/components/ui/button'
+import { trpc, getQueryClient } from '@/trpc/server'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { Client } from './client'
+import { Suspense } from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: 'filway PREFETCH' })
+  )
+
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   )
 }
 
