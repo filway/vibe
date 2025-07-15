1869ef4c87be356d1dae7f2dbc7de0320d990421
zhouxiao <zhouxiao@npn.sg>
2025-07-15 16:53:55 +0800

    04: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
-import { trpc, getQueryClient } from '@/trpc/server'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-import { Client } from './client'
-import { Suspense } from 'react'
+'use client'
 
-const Page = async () => {
-  const queryClient = getQueryClient()
-  void queryClient.prefetchQuery(
-    trpc.createAI.queryOptions({ text: 'filway PREFETCH' })
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
+const Page = () => {
+  const trpc = useTRPC()
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success('Background job started')
+      },
+    })
   )
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => invoke.mutate({ text: 'filway' })}>
+        Invoke Background Job
+      </Button>
+    </div>
   )
 }
 
